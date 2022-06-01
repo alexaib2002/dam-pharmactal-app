9ef97e34c7dadd2985f48b1e278575b8d7ef72dd
deveshaha <devesh_hanumante@outlook.com>
2022-06-01 10:29:27 +0200

    updated ddbb

--- src/org/uem/dam/GestorFarmacia/db/DBAccess.java
+++ src/org/uem/dam/GestorFarmacia/db/DBAccess.java
@@ -1,8 +1,13 @@
 package org.uem.dam.GestorFarmacia.db;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBAccess {
 
@@ -10,9 +15,28 @@ public class DBAccess {
 	private String url;
 	
 	public DBAccess() {
-		driver = "org.sqlite.JDBC";
-		url = "jdbc:sqlite:db/ddbb_GestorFarmacia.db";
+		Properties prop = new Properties();
+		InputStream is = null;
+		
+		try {
+			is = new FileInputStream("db/ConfigurationDB.properties");
+			prop.load(is);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (is != null) {
+					is.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+			
+		
 	
 	public Connection getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName(driver);
