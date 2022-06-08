5db2302f439fbedd4e2f7c54094e5e15061aea29
alexaib <alexaib@outlook.es>
2022-06-09 00:13:20 +0200

    calls to TableSubmenu.updateTable implemented on TabbedSubmenu

--- src/org/uem/dam/GestorFarmacia/view/submenus/TabbedSubmenu.java
+++ src/org/uem/dam/GestorFarmacia/view/submenus/TabbedSubmenu.java
@@ -4,17 +4,21 @@ import java.awt.event.ActionListener;
 import java.util.LinkedHashMap;
 
 import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.uem.dam.GestorFarmacia.contract.ArticleContract;
 import org.uem.dam.GestorFarmacia.contract.MedContract;
 import org.uem.dam.GestorFarmacia.contract.ProviderContract;
 import org.uem.dam.GestorFarmacia.contract.TableContract;
+import org.uem.dam.GestorFarmacia.model.DBItem;
 
 import net.miginfocom.swing.MigLayout;
 
 public class TabbedSubmenu extends DefaultSubmenu {
 
-	private LinkedHashMap<String, TableSubmenu> tabContainer;
+	private static final long serialVersionUID = 1L;
+	private LinkedHashMap<String, TableSubmenu> tabContainerPointer;
 	private JTabbedPane tabbedPane;
 
 	public TabbedSubmenu() {
@@ -24,12 +28,23 @@ public class TabbedSubmenu extends DefaultSubmenu {
 	@Override
 	public void initComponents() {
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addChangeListener((event) -> {
+			try {
+				String selectedTable = tabbedPane.getSelectedComponent().getName();
+				System.err.println("[FIXME] Table updating not implemented");
+				// FIXME until queries to ddbb are implemented, this code won't work
+				DBItem[] queryResult = null;
+				tabContainerPointer.get(selectedTable).updateTable(queryResult);
+			} catch (NullPointerException npe) {
+				System.err.println("Fatal error, cannot fetch data from DDBB");
+			}
+		});
 		add(tabbedPane, "cell 0 0,grow");
 	}
 
 	@Override
 	public void initAttributes() {
-		tabContainer = new LinkedHashMap<>();
+		tabContainerPointer = new LinkedHashMap<>();
 		String[] cols;
 		TableSubmenu tableSubmn;
 		for (String tableName : TableContract.getUITables()) {
@@ -37,6 +52,7 @@ public class TabbedSubmenu extends DefaultSubmenu {
 			tableSubmn = new TableSubmenu(cols);
 			tableSubmn.setName(tableName);
 			tabbedPane.add(tableSubmn);
+			tabContainerPointer.put(tableName, tableSubmn);
 		}
 
 	}
