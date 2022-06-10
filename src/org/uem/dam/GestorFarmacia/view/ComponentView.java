88fe3f72e67ebc1b42a4a5db6932a333251dd4ae
alexaib <alexaib@outlook.es>
2022-06-10 20:08:30 +0200

    removed unused method on ComponentView

--- src/org/uem/dam/GestorFarmacia/view/ComponentView.java
+++ src/org/uem/dam/GestorFarmacia/view/ComponentView.java
@@ -1,16 +1,9 @@
 package org.uem.dam.GestorFarmacia.view;
 
-import java.awt.event.ActionListener;
-import java.util.EventListener;
-
-import javax.swing.event.ChangeListener;
-
 public interface ComponentView {
 
 	public void initComponents();
 
 	public void initAttributes();
-
-	public void updateListeners(ActionListener controller);
 
 }
