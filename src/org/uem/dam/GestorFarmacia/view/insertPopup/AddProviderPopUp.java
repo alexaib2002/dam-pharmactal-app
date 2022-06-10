3128d4375a51bc459da1843800850c7ac9dc34a2
deveshaha <devesh_hanumante@outlook.com>
2022-06-10 12:26:06 +0200

    create insert Provider class

---
+++ src/org/uem/dam/GestorFarmacia/view/insertPopup/AddProviderPopUp.java
@@ -0,0 +1,19 @@
+package org.uem.dam.GestorFarmacia.view.insertPopup;
+
+import java.awt.event.ActionListener;
+
+import org.uem.dam.GestorFarmacia.view.ComponentView;
+import org.uem.dam.GestorFarmacia.view.submenus.DefaultSubmenu;
+
+public class AddProviderPopUp extends DefaultSubmenu implements ComponentView {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void updateListeners(ActionListener controller) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
