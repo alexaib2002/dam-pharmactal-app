633fa964807ecee26d77d14f42580a836adffadf
alexaib <alexaib@outlook.es>
2022-06-12 00:36:57 +0200

    updated welcome page to comply with main branch

--- src/org/uem/dam/GestorFarmacia/view/submenus/welcome/WelcomePage.java
+++ src/org/uem/dam/GestorFarmacia/view/submenus/welcome/WelcomePage.java
@@ -1,18 +1,19 @@
-package org.uem.dam.GestorFarmacia.view.welcome;
+package org.uem.dam.GestorFarmacia.view.submenus.welcome;
 
-import java.awt.event.ActionListener;
-import org.uem.dam.GestorFarmacia.view.ComponentView;
-import org.uem.dam.GestorFarmacia.view.submenus.DefaultSubmenu;
-
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import org.uem.dam.GestorFarmacia.view.BuildableView;
+import org.uem.dam.GestorFarmacia.view.submenus.DefaultInteractableSubmenu;
 
 import net.miginfocom.swing.MigLayout;
-import javax.swing.JButton;
 
-public class WelcomePage extends DefaultSubmenu<ActionListener> implements ComponentView {
+public class WelcomePage extends DefaultInteractableSubmenu<ActionListener> implements BuildableView {
 	public WelcomePage() {
 		initComponents();
 	}
@@ -23,23 +24,23 @@ public class WelcomePage extends DefaultSubmenu<ActionListener> implements Compo
 	@Override
 	public void initComponents() {
 		setLayout(new MigLayout("", "[163px,grow,center]", "[][][183.00,grow][grow]"));
-		
+
 		JLabel lblWelcome = new JLabel("Welcome to Pharmactal");
 		lblWelcome.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(lblWelcome, "cell 0 0");
-		
+
 		JLabel lblLogo = new JLabel("");
 		Image logo = new ImageIcon(this.getClass().getResource("/pharmactal.png")).getImage();
-		
+
 		JLabel lblNewLabel = new JLabel("The best pharmacy manager");
 		add(lblNewLabel, "cell 0 1");
 		lblLogo.setIcon(new ImageIcon(logo));
 
 		add(lblLogo, "cell 0 2,alignx center,aligny center");
-		
+
 		btnContinue = new JButton("Continue");
 		add(btnContinue, "cell 0 3,alignx center");
-		
+
 	}
 
 	@Override
