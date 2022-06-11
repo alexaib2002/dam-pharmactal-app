83f399ff82578c46339e598fd4963d314312b260
deveshaha <devesh_hanumante@outlook.com>
2022-06-11 20:45:17 +0200

    welcome page implementation

--- src/org/uem/dam/GestorFarmacia/view/welcome/WelcomePage.java
+++ src/org/uem/dam/GestorFarmacia/view/welcome/WelcomePage.java
@@ -10,6 +10,7 @@ import java.awt.Font;
 import java.awt.Image;
 
 import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
 
 public class WelcomePage extends DefaultSubmenu<ActionListener> implements ComponentView {
 	public WelcomePage() {
@@ -17,28 +18,33 @@ public class WelcomePage extends DefaultSubmenu<ActionListener> implements Compo
 	}
 
 	private static final long serialVersionUID = 1L;
+	private JButton btnContinue;
 
 	@Override
 	public void initComponents() {
-		setLayout(new MigLayout("", "[163px,grow,center]", "[][grow]"));
+		setLayout(new MigLayout("", "[163px,grow,center]", "[][][183.00,grow][grow]"));
 		
-		JLabel lblWelcome = new JLabel("¡Bienvenido Usuario!");
+		JLabel lblWelcome = new JLabel("Welcome to Pharmactal");
 		lblWelcome.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(lblWelcome, "cell 0 0");
 		
+		JLabel lblLogo = new JLabel("");
+		Image logo = new ImageIcon(this.getClass().getResource("/pharmactal.png")).getImage();
 		
-		
-	}
+		JLabel lblNewLabel = new JLabel("The best pharmacy manager");
+		add(lblNewLabel, "cell 0 1");
+		lblLogo.setIcon(new ImageIcon(logo));
 
-	@Override
-	public void initAttributes() {
-		// TODO Auto-generated method stub
+		add(lblLogo, "cell 0 2,alignx center,aligny center");
+		
+		btnContinue = new JButton("Continue");
+		add(btnContinue, "cell 0 3,alignx center");
+		
 		
 	}
 
 	@Override
 	public void updateListeners(ActionListener controller) {
-		// TODO Auto-generated method stub
-		
+		btnContinue.addActionListener(controller);
 	}
 }
