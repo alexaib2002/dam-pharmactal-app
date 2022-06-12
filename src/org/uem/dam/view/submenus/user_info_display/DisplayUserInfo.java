817675b89470c5f9c4bbe5eb5b57682e6b93af65
deveshaha <devesh_hanumante@outlook.com>
2022-06-12 16:30:20 +0200

    create DisplayUserInfo Class

---
+++ src/org/uem/dam/view/submenus/user_info_display/DisplayUserInfo.java
@@ -0,0 +1,30 @@
+package org.uem.dam.view.submenus.user_info_display;
+
+import java.awt.event.ActionListener;
+
+import org.uem.dam.GestorFarmacia.view.BuildableView;
+import org.uem.dam.GestorFarmacia.view.submenus.DefaultInteractableSubmenu;
+
+public class DisplayUserInfo extends DefaultInteractableSubmenu<ActionListener> implements BuildableView  {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void initComponents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initAttributes() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateListeners(ActionListener controller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
