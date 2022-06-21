9bbf2de6ad0526729fb7eea24a67321119882f9d
alexaib <alexaib@outlook.es>
2022-06-21 18:04:34 +0200

    foreign key enforcement implemented

--- src/org/uem/dam/gestor_farmacia/persist/DBConnection.java
+++ src/org/uem/dam/gestor_farmacia/persist/DBConnection.java
@@ -8,12 +8,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import org.sqlite.SQLiteConfig;
 import org.uem.dam.gestor_farmacia.utils.ErrorUtils;
 
 public class DBConnection {
 
 	private String driver;
 	private String url;
+	private SQLiteConfig config;
 
 	public DBConnection() {
 		FileInputStream fileInputStream;
@@ -26,22 +28,29 @@ public class DBConnection {
 		} catch (FileNotFoundException e) {
 			ErrorUtils.onFatalErrorException(
 					"El fichero de configuración de la BBDD (dbconfig.properties)"
-							+ "no ha sido encontrado en la ruta del ejecutable.");
+							+ "no ha sido encontrado en la ruta del ejecutable."
+			);
 		} catch (IOException e) {
 			ErrorUtils.onFatalErrorException("Se ha producido un fallo en la Entrada/Salida de la aplicación");
 		}
+		setupSQLConfig();
 	}
 
 	public Connection getConnection() {
 		try {
 			Class.forName(driver);
-			return DriverManager.getConnection(url);
+			return DriverManager.getConnection(url, config.toProperties());
 		} catch (ClassNotFoundException e) {
 			System.err.println("El driver o la URL no se han podido cargar correctamente en el gestor de conexiones");
 		} catch (SQLException e) {
 			System.err.println("Excepcion SQL al obtener la conexion a la BBDD");
 		}
 		return null;
+	}
+
+	private void setupSQLConfig() {
+		config = new SQLiteConfig();
+		config.enforceForeignKeys(true);
 	}
 
 }
